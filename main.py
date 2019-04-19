89ae7ed52add7fe0c4cc2d47bb4f34ad6cd66010
Aditya Mittal <mittal.adi333@gmail.com>
2019-04-19 16:47:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,41 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 19 16:40:45 2019
+
+@author: MitTal
+"""
+
 import cv2
 import numpy as np
 import argparse
 from matplotlib import pyplot as plt
 from l0smooth.L0_serial import l0_smooth
 
+
+image = cv2.imread('images\\img.png')
+gray_img = cv2.cvtColor(image ,cv2.COLOR_BGR2GRAY)
+image_l0 = l0_smooth(image)
+image_blur = cv2.GaussianBlur(image_l0, (5, 5), 0)
+img_can = cv2.Canny(image_blur.astype('uint8'), 100, 200)
+
+gray_blur = cv2.cvtColor(image_blur,cv2.COLOR_BGR2GRAY)
 '''
-img = cv2.imread('img2.png')
-edges = cv2.Canny(img,100,200)
+khali_image = np.zeros((232,230))
+for i in range(0,len(khali_image)):
+for j in range(0,len(khali_image[0])):
+if(img_can[i][j]==255):
+khali_image[i][j] = gray_blur[i][j]
+'''         
 
-plt.subplot(121),plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-plt.title('Original Image'), plt.xticks([]), plt.yticks([])
-plt.subplot(122),plt.imshow(edges, cmap='gray')
-plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+image_I2 = np.zeros((232,230))
 
-edges = cv2.Canny(image_l0.astype('uint8'), 100, 200)
+for i in range(0,len(image_I2)):
+    for j in range(0,len(image_I2[0])):
 
-plt.show()
-'''
-def main():
+        if(gray_blur[i][j] == 0):
+            delta_I0 = 255 
+        else:
+            delta_I0 = ((255-gray_blur[i][j])/gray_blur[i][j])*gray_img[i][j]
 
-    parser = argparse.ArgumentParser(description="mural2sketch")
+        image_I2[i][j] = min(255, (gray_img[i][j] + delta_I0))
 
-    parser.add_argument('image', help="input image file")
-
-    args = parser.parse_args()
-
-    image = args.image
-
-    # Preprocessing
-
-    image_l0 = l0_smooth(image)
-
-    image_blur = cv2.GaussianBlur(image_l0, (5, 5), 0)
-
-    # Outer Edge Detection
-
-    image_can = cv2.Canny(image_blur.astype('uint8'), 100, 200)
-
-if __name__ == '__main__':
-    main()
+cv2.imwrite('abc.png',image_I2)
